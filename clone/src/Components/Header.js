eeec851dcae3b3ba066399e308129b5d0d85d887
ThomasCzacho <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-07-27 16:02:55 +0000

    Icon Commit

--- clone/src/Components/Header.js
+++ clone/src/Components/Header.js
@@ -1,12 +1,46 @@
 import Typography from '@mui/material/Typography';
 import AppBar from '@mui/material/AppBar';
 //a import Box from '@mui/material/Box';
-//a import Container from '@mui/material/Container';
+import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import { Avatar } from '@mui/material';
 //a import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import EmailIcon from '@mui/icons-material/Email';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#E3000B',
+    color: '#E3000B',
+    boxShadow: `0 0 0 1px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '100%',
+      
+      border: '',
+      content: '""',
+    },
+  },
+}));
+
+const theme = createTheme({
+  typography: {
+    button: {
+      fontSize: '1rem',
+    },
+  },
+});
 
 export default function Header() {
     return (
@@ -35,17 +69,44 @@ export default function Header() {
         variant="square"
         
 />
-<Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="contained"
+<Container>
+<TextField
+            hiddenLabel
+            id="filled-hidden-label-normal"
+            variant="filled"
+            label='Search'
+            backgroundColor='#ffffff'
+            borderColor="#ffffff"
           />
-        </Grid>
+          </Container>
+          
+          
+            <Stack align='right' container direction='rows' spacing={20}
+            >
+              <StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        variant="dot"
+      >
+            <EmailIcon sx={{ width: 28, height: 28 }} ></EmailIcon></StyledBadge>
+
+<StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        variant="dot"
+      >
+<NotificationsIcon sx={{ width: 28, height: 28 }} ></NotificationsIcon></StyledBadge>
+
+<Avatar
+        alt="Remy Sharp"
+        
+        sx={{ width: 28, height: 28 }}
+        
+        
+><PersonIcon></PersonIcon></Avatar>
+</Stack>
+
+         
     </Toolbar>
     </AppBar>
     )
